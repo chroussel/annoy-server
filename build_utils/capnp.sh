1e17bfa24dfa8fd3e5855ce9f7cc4cfcb124b6df
Charles Roussel <c.roussel@criteo.com>
2018-11-26 23:05:32 +0100

    Add script to install capnproto

---
+++ build_utils/capnp.sh
@@ -0,0 +1,12 @@
+mkdir -p build/
+
+(
+    cd build/
+    curl -O https://capnproto.org/capnproto-c++-0.7.0.tar.gz
+    tar zxf capnproto-c++-0.7.0.tar.gz
+    cd capnproto-c++-0.7.0
+    ./configure
+    make -j6 check
+    sudo make install
+)
+
